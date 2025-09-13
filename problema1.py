3de5a90615967c7b7cf5075aa74eb7dddd359d5e
bojorquezja <bojorquez.ja@gmail.com>
2025-09-12 20:30:44 -0500

    prob 1

---
+++ problema1.py
@@ -0,0 +1,7 @@
+"""
+Usando la clase Perro, agregue el atributo edad y 
+haga un menu con estas opciones:
+1) crear 1 perro y agregarlos a una lista de perros existente
+2) mostrar todos los perros de la lista
+3) fin
+"""
